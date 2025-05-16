6a5fbaa2567c916d971d98434eedab3d75305d34
Jonas Reucher <jonas@arkonatech.com>
2025-05-16 09:51:00 +0200

    update build script, bundle /web

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
-npx esbuild src/server.ts --bundle --platform=node --define:DEBUG=true --sourcemap --outfile=build/server.mjs --format=esm --banner:js='import {createRequire} from "module"; const require = createRequire(import.meta.url);'
+npx esbuild src/server.ts --bundle --platform=node --define:DEBUG=true --sourcemap --outfile=build/server/server.mjs --format=esm --banner:js='import {createRequire} from "module"; const require = createRequire(import.meta.url);'
+cp -r ./web ./build/server/web
 
