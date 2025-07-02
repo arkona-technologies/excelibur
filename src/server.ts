32680d4263b886f8f64d8690c0298283e675d560
Jonas Reucher <jonas@arkonatech.com>
2025-07-02 17:52:29 +0200

    feat(web): add .xlsx support for webui

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,21 @@ import FastifyMultipart from "@fastify/multipart";
 import FormBody from "@fastify/formbody";
 import * as VAPI from "vapi";
 import Fastify from "fastify";
+import xlsx from "node-xlsx";
 import path from "path";
 import { open_connection } from "./connection.js";
 import { parse_csv } from "./csv.js";
 import {
   ProcessingChainConfig,
   ReceiverConfig,
+  refine_config,
   SenderConfig,
 } from "./zod_types.js";
 import { apply_senders_config } from "./senders.js";
 import { setup_processing_chains } from "./processors.js";
 import { apply_receivers_config } from "./receivers.js";
 import { base } from "./base.js";
-import { enforce_nonnull } from "vscript";
+import { enforce, enforce_nonnull } from "vscript";
 import cors from "@fastify/cors";
 function stream_to_string(stream: any): Promise<string> {
   const chunks: any[] = [];
@@ -35,7 +37,7 @@ const fastify = Fastify({
 fastify.addContentTypeParser(
   "application/json",
   { parseAs: "string" },
-  function(_req: any, body: any, done: any) {
+  function (_req: any, body: any, done: any) {
     try {
       var json: any = JSON.parse(body);
       done(null, json);
@@ -46,12 +48,12 @@ fastify.addContentTypeParser(
   },
 );
 
-fastify.addContentTypeParser("text/csv", function(_req, payload, done) {
+fastify.addContentTypeParser("text/csv", function (_req, payload, done) {
   let body = "";
-  payload.on("data", function(data) {
+  payload.on("data", function (data) {
     body += data;
   });
-  payload.on("end", function() {
+  payload.on("end", function () {
     try {
       done(null, body);
     } catch (e) {
@@ -101,8 +103,43 @@ fastify.post("/processor-config", async (req, _res) => {
   await setup_processing_chains(vm, processors_config);
   return `Done`;
 });
+
+fastify.post("/excel", async (req, _res) => {
+  const maybe_excel = enforce_nonnull(await req.file().then(f=>f?.toBuffer()));
+  enforce(!!maybe_excel);
+
+  let raw_config;
+  let tx_config;
+  let rx_config;
+  const parsed_excel = xlsx.parse(maybe_excel);
+  for (const sheet of parsed_excel) {
+    console.log(`Parsing ${sheet.name}: ${sheet.data.length} lines`);
+    const as_csv = sheet.data
+      .filter((row, _idx, rows) => row.length == rows[0].length)
+      .map((row) => row.join(","))
+      .join("\n");
+    if (sheet.name === "PROC") {
+      raw_config = parse_csv(as_csv, ProcessingChainConfig);
+    }
+    if (sheet.name === "TX") {
+      tx_config = parse_csv(as_csv, SenderConfig);
+      console.log(tx_config);
+    }
+    if (sheet.name === "RX") {
+      rx_config = parse_csv(as_csv, ReceiverConfig);
+      console.log(rx_config);
+    }
+  }
+  enforce(!!raw_config && !!tx_config && !!rx_config);
+  const processors_config = refine_config(raw_config);
+  await base(vm);
+  await setup_processing_chains(vm, processors_config);
+  await apply_senders_config(vm, tx_config);
+  await apply_receivers_config(vm, rx_config);
+  return `Done`;
+});
 fastify.listen(
-  { port: parseInt(process.env["PORT"] ?? "4242"), host: "0.0.0.0" },
+  { port: parseInt(process.env["PORT"] ?? "4242 "), host: "0.0.0.0" },
   (err, addr) => {
     if (err) {
       console.log(err);
