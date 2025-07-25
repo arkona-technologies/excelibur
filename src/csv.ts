fd2df44fa7521707afb96d09abc8fc47a3cd3347
Jonas Reucher <jonas@arkonatech.com>
2025-07-25 16:48:44 +0200

    fix(csv): fix csv parser canceling if matches rows don't exactly match...

--- src/csv.ts
+++ src/csv.ts
@@ -8,7 +8,7 @@ export function parse_csv<T extends z.ZodRawShape>(
 ): z.infer<typeof schema>[] {
   const is_schema = (row: string[], schema: z.ZodObject<T>) => {
     const keys = Object.keys(schema.shape);
-    const matches = keys.filter((k)=> row.includes(k)).length;
+    const matches = keys.filter((k) => row.includes(k)).length;
     return matches > 3; // ehhh
   };
   row_sep ??= "\n";
@@ -17,18 +17,21 @@ export function parse_csv<T extends z.ZodRawShape>(
     .split(row_sep)
     .map((row) => row.split(",").flatMap((entry) => entry.trim()))
     .find((row_split) => is_schema(row_split, schema));
-  enforce(!!csv_header, "Need a valid csv header to determine parameter mapping!");
+  enforce(
+    !!csv_header,
+    "Need a valid csv header to determine parameter mapping!",
+  );
   csv_header.forEach((key, idx) => {
     parameter_mapping[key] = idx;
   });
-  console.log('CSV Parser found mapping: ', parameter_mapping);
+  console.log("CSV Parser found mapping: ", parameter_mapping);
 
   const rows = csv
     .split(row_sep)
     .map((row) => row.split(",").flatMap((entry) => entry.trim()))
     .filter((row_split) => !is_schema(row_split, schema))
-    .filter((row_split) => row_split.length === Object.keys(parameter_mapping).length)
     .map((r) => r.map((c) => (c == "" ? null : c)));
+  console.log("CSV Parser: ", rows.length, "matching lines");
   const parsed = rows
     .map((row) => {
       try {
@@ -43,8 +46,6 @@ export function parse_csv<T extends z.ZodRawShape>(
         return null;
       }
     })
-    .filter((nullable) => !!nullable); 
+    .filter((nullable) => !!nullable);
   return parsed;
 }
-
-
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { parse_csv } from "./csv.js";
 import * as VAPI from "vapi";
 
 import fs from "fs";
-import {  enforce, enforce_nonnull, } from "vscript";
+import { enforce, enforce_nonnull } from "vscript";
 import { open_connection } from "./connection.js";
 import { base } from "./base.js";
 import {
@@ -28,7 +28,7 @@ if (excel) {
   for (const sheet of parsed_excel) {
     console.log(`Parsing ${sheet.name}: ${sheet.data.length} lines`);
     const as_csv = sheet.data
-      .filter((row, _idx, rows) => row.length == rows[0].length)
+      .filter((row) => row.length > 0)
       .map((row) => row.join(","))
       .join("\n");
     if (sheet.name === "PROC") {
@@ -36,11 +36,9 @@ if (excel) {
     }
     if (sheet.name === "TX") {
       tx_config = parse_csv(as_csv, SenderConfig);
-      console.log(tx_config);
     }
     if (sheet.name === "RX") {
       rx_config = parse_csv(as_csv, ReceiverConfig);
-      console.log(rx_config);
     }
   }
 }
@@ -56,7 +54,7 @@ if (!excel) {
   tx_config = parse_csv(tx, SenderConfig);
   rx_config = parse_csv(rx, ReceiverConfig);
 }
-enforce(!!raw_config && !!tx_config && !!rx_config)
+enforce(!!raw_config && !!tx_config && !!rx_config);
 const processors_config = refine_configs(raw_config);
 const vm = (await open_connection(
   new URL(process.env["URL"] ?? "ws://127.0.0.1"),
